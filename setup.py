097c7c57a62f3adb86677d6467639f5f36c95070
aromemusa <musa@fbn-dummerstorf.de>
2025-04-07 15:09:23 +0200

    Set up dev branch with pinned dependencies and msq.py

--- setup.py
+++ setup.py
@@ -1,39 +1,37 @@
 import setuptools
 
-with open("README.md", "r", newline="", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-
 
 setuptools.setup(
     name="PyMSQ",
-    packages=setuptools.find_packages(),
     version="0.1.1",
-    license="MIT",
+    author="Abdulraheem Musa, Norbert Reinsch",
+    author_email="musa@fbn-dummerstorf.de, reinsch@fbn-dummerstorf.de",
     description="A Python package for estimating Mendelian sampling-related quantities",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="Abdulraheem Musa, Norbert Reinsch",
-    author_email="musa@fbn-dummerstorf.de, reinsch@fbn-dummerstorf.de",
     url="https://github.com/aromemusa/PyMSQ",
-    download_url="https://github.com/aromemusa/PyMSQ/blob/main/dist/PyMSQ-0.1.tar.gz",
+    packages=setuptools.find_packages(),        # Ensures PyMSQ/msq.py is included
+    license="MIT",
+    download_url="https://github.com/aromemusa/PyMSQ/dist/PyMSQ-0.1.2.tar.gz",
     keywords=["Mendelian sampling", "variance", "covariance", "similarity", "selection", "haplotype diversity"],
+    python_requires=">=3.8",
     install_requires=[
-        "numpy<1.25",
+        "numpy<1.25",       # pinning to avoid conflicts with Numba
         "pandas",
         "scipy",
-        "numba<0.58",
+        "numba<0.58",       # pin Numba to a version that works with the above
     ],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Education",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.8",
     include_package_data=True,
     package_data={"": ["data/*.txt"]},
 )
